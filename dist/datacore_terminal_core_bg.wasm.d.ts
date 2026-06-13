28f9018dcacf328d50b48a04737a16fe522cbbf6
beto <github@beto.group>
2026-06-14 06:38:59 +0800

    chore: add wasm build artifacts to repository

---
+++ dist/datacore_terminal_core_bg.wasm.d.ts
@@ -0,0 +1,20 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export const start_terminal: (a: number, b: number) => [number, number];
+export const wasm_bindgen__convert__closures_____invoke__h5c819e0689d1fec7: (a: number, b: number, c: any) => [number, number];
+export const wasm_bindgen__convert__closures_____invoke__h1ebfa33a152c3d41: (a: number, b: number, c: any) => void;
+export const wasm_bindgen__convert__closures_____invoke__h15d2aa57c862f906: (a: number, b: number, c: any) => void;
+export const wasm_bindgen__convert__closures_____invoke__h1ebfa33a152c3d41_3: (a: number, b: number, c: any) => void;
+export const wasm_bindgen__convert__closures_____invoke__h1ebfa33a152c3d41_4: (a: number, b: number, c: any) => void;
+export const wasm_bindgen__convert__closures_____invoke__h102762f7ec5523b6: (a: number, b: number) => [number, number];
+export const wasm_bindgen__convert__closures_____invoke__h8669f312916f4414: (a: number, b: number) => void;
+export const __wbindgen_malloc: (a: number, b: number) => number;
+export const __wbindgen_realloc: (a: number, b: number, c: number, d: number) => number;
+export const __wbindgen_exn_store: (a: number) => void;
+export const __externref_table_alloc: () => number;
+export const __wbindgen_externrefs: WebAssembly.Table;
+export const __wbindgen_free: (a: number, b: number, c: number) => void;
+export const __wbindgen_destroy_closure: (a: number, b: number) => void;
+export const __externref_table_dealloc: (a: number) => void;
+export const __wbindgen_start: () => void;
